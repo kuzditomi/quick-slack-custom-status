bbb1594e96e368b716974f5f11ddf5544da1bdf7
Küzdi Tamás <kuzditomi@gmail.com>
2020-04-20 20:30:38 +0200

    add saving

--- QuickSlackStatusUpdate/wwwroot/js/site.js
+++ QuickSlackStatusUpdate/wwwroot/js/site.js
@@ -1,6 +1,75 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const STORAGE_KEY = 'slack-statuses';
 
-// Write your JavaScript code.
-window.loadSavedStatuses = () => {
-}
+window.initStatusScript = () => {
+    const savedStatuses = loadStatuses();
+    createList(savedStatuses);
+
+    $(document).on('click', '#add', () => {
+        const text = $('#statustext').val();
+        const emoji = $('#statusemoji').val();
+
+        if (text && emoji) {
+            const savedStatuses = loadStatuses();
+
+            savedStatuses.push({
+                text,
+                emoji
+            });
+
+            saveStatuses(savedStatuses);
+            createList(savedStatuses);
+
+            $('#statustext').val('');
+            $('#statusemoji').val('');
+        }
+    });
+
+    $(document).on('click', '.update-status', (evt) => {
+        const $btn = $(evt.target);
+
+        const text = $btn.data('text');
+        const emoji = $btn.data('emoji');
+
+        if (text && emoji) {
+            const linkId = $('[name=linkId]').val();
+            const statustext = encodeURIComponent(text);
+            const statusemoji = encodeURIComponent(emoji);
+
+            $.post(`/api/slack/status?linkId=${linkId}&statustext=${statustext}&statusemoji=${statusemoji}`);
+        }
+    });
+
+
+    function loadStatuses() {
+        const savedStatuses = localStorage.getItem(STORAGE_KEY);
+
+        if (!savedStatuses) {
+            return [];
+        }
+
+        return JSON.parse(savedStatuses) || [];
+    }
+
+    function saveStatuses(statuses) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(statuses || []));
+    }
+
+    function createList(savedStatuses) {
+        $('#saved-statuses').html('');
+
+        for (const status of savedStatuses) {
+            const $listElement = $('<div/>');
+            const $label = $('<label>').text(`${status.text} (${status.emoji})`);
+            const $button = $('<button>')
+                .addClass('update-status')
+                .text('update!')
+                .data('text', status.text)
+                .data('emoji', status.emoji);
+
+            $listElement.append($label);
+            $listElement.append($button);
+
+            $('#saved-statuses').append($listElement);
+        }
+    }
+};
